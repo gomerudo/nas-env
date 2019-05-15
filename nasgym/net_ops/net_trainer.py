Fix wrong shape obtainment

nasgym/net_ops/net_trainer.py
@@ -343,7 +343,7 @@ class EarlyStopNASTrainer(DefaultNASTrainer):
         # Define the model_fn we want to return
         def model_fn(features, labels, mode):
             with tf.variable_scope(self.variable_scope):
-                print("Shape of features is:", features.get_shape().dims)
+                print("Shape of features is:", features["x"].get_shape().dims)
                 print("Expected shape of features is:", self.input_shape)
                 # 1. Define the input placeholder
                 if len(self.input_shape) == 2:  # Reshape if necessary