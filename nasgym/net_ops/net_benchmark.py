442e9e2468917fa5dcaae9c466c99a8ad798215b
Jorge Gomez <gomerudo@gmail.com>
2019-09-14 01:32:03 +0200

    Reduce depth of vgg to avoid zeros

--- nasgym/net_ops/net_benchmark.py
+++ nasgym/net_ops/net_benchmark.py
@@ -116,38 +116,38 @@ def vgg_net_builder(input_tensor):
             # padding="same"
         )(current_layer)
 
-    with tf.name_scope("Block4"):
-        # Conv 1
-        current_layer = tf.keras.layers.Conv2D(
-            filters=512,
-            kernel_size=(3, 3),
-            padding="same",
-            name="Conv1"
-        )(current_layer)
+    # with tf.name_scope("Block4"):
+    #     # Conv 1
+    #     current_layer = tf.keras.layers.Conv2D(
+    #         filters=512,
+    #         kernel_size=(3, 3),
+    #         padding="same",
+    #         name="Conv1"
+    #     )(current_layer)
 
-        current_layer = tf.keras.layers.ReLU(
-            name="ReLU"
-        )(current_layer)
+    #     current_layer = tf.keras.layers.ReLU(
+    #         name="ReLU"
+    #     )(current_layer)
 
-        # Conv 2
-        current_layer = tf.keras.layers.Conv2D(
-            filters=512,
-            kernel_size=(3, 3),
-            padding="same",
-            name="Conv2"
-        )(current_layer)
+    #     # Conv 2
+    #     current_layer = tf.keras.layers.Conv2D(
+    #         filters=512,
+    #         kernel_size=(3, 3),
+    #         padding="same",
+    #         name="Conv2"
+    #     )(current_layer)
 
-        current_layer = tf.keras.layers.ReLU(
-            name="ReLU"
-        )(current_layer)
+    #     current_layer = tf.keras.layers.ReLU(
+    #         name="ReLU"
+    #     )(current_layer)
 
-        # Pooling
-        current_layer = tf.keras.layers.MaxPool2D(
-            pool_size=(2, 2),
-            strides=(2, 2),
-            name="MaxPooling",
-            # padding="same"
-        )(current_layer)
+    #     # Pooling
+    #     current_layer = tf.keras.layers.MaxPool2D(
+    #         pool_size=(2, 2),
+    #         strides=(2, 2),
+    #         name="MaxPooling",
+    #         # padding="same"
+    #     )(current_layer)
 
     with tf.name_scope("DenseBlock"):
         current_layer = tf.layers.flatten(
